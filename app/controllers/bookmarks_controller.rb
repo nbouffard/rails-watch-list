7d4d565a6f4d0323566cc9053025eedfd39f7309
nicola bouffard <nicola.bouffard855@gmail.com>
2023-05-24 14:06:27 +0200

    add destroy bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,5 @@
 class BookmarksController < ApplicationController
-  before_action :set_list
+  before_action :set_list, only: %i[new create]
 
   def new
     @bookmark = Bookmark.new
@@ -13,6 +13,13 @@ class BookmarksController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    list_id = @bookmark.list_id
+    @bookmark.destroy
+    redirect_to list_path(list_id), status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create]
   end
+  resources :bookmarks, only: [:destroy]
 end
